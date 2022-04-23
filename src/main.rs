aaadef675d816a58e5b0d0ad6cac172142bee7db
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-04-23 21:51:13 +1000

    Add stuff

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@ fn main() {
     println!("Hello, Eletia!");
 
     // setup config
+
+    // only load the cards that the player has from assets/user_state.yml
+    // and any that is on display
+
+    // when duelling enemy
+    // load the enemy cards too, from assets/cards
 }
 
 use terraformer3d::Camera3D;
